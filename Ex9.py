132317dd589ce3912a6c9f3a0537c01864903ce4
saurontl <61851698+saurontl@users.noreply.github.com>
2020-03-21 11:01:14 -0300

    Delete Ex9.py

--- Ex9.py
+++
@@ -1,5 +0,0 @@
-#9. Escreva um programa que converte valores de polegadas em centímetros utilizando a seguinte fórmula para conversão:
-#1 polegada = 2,54 cm;
-polegadas = float(input('Digite o valor em polegadas: '))
-cm = float(polegadas * 2.54)
-print(f'{polegadas:.2f} polegadas = {cm:.2f}cm')
