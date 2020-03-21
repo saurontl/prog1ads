e2b3d9a22e4b14289d75d0075cc9b2de8aadfaaa
saurontl <61851698+saurontl@users.noreply.github.com>
2020-03-21 11:00:12 -0300

    Delete Ex4.py

--- Ex4.py
+++
@@ -1,7 +0,0 @@
-#4. Escreva um programa que leia uma temperatura em graus Fahrenheit, transforme-a em graus Celsius e exiba o resultado.
-fahrenheit = float(input('Digite o valor em Farenheit: '))
-celsius = (fahrenheit - 32) / 1.8
-print(f'{fahrenheit}ºF = {celsius}ºC')
-
-
-
