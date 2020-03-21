06608affc8fbfeb3ccfe8a8bd191758959c72716
saurontl <61851698+saurontl@users.noreply.github.com>
2020-03-21 11:01:04 -0300

    Delete Ex8.py

--- Ex8.py
+++
@@ -1,4 +0,0 @@
-#8. Escreva um programa que leia um valor inteiro e calcule o seu cubo.
-valor_inteiro = int(input('Digite o valor inteiro a ser calculado: '))
-valor_cubico = int(valor_inteiro ** 3)
-print(f'O cubo de {valor_inteiro} é {valor_cubico}.')
