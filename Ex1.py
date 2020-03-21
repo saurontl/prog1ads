5e5ef7c4926c189c78147139004b5c0a534910a0
saurontl <61851698+saurontl@users.noreply.github.com>
2020-03-21 10:59:16 -0300

    Delete Ex1.py

--- Ex1.py
+++
@@ -1,9 +0,0 @@
-#José Everton da Silva Filho
-#AdS - P1 - Unifip - Patos/PB
-#####
-#1. Faça um programa que solicite ao usuário o valor do litro de combustível (ex. 4,75) e quanto em dinheiro ele deseja
-# abastecer (ex. 50,00). Calcule quantos litros de combustível o usuário obterá com esses valores.
-combustivel = float(input("Digite o valor em reais do litro do combustível: R$"))
-dinheiro = float(input("Digite o valor em reais que você deseja abastecer: R$"))
-total = combustivel * dinheiro
-print(f"Você abastecerá {total:.2f} litros.")
