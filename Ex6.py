ebac498bf100adfa0f3cf56a055a5d320e2b0272
saurontl <61851698+saurontl@users.noreply.github.com>
2020-03-21 11:00:40 -0300

    Delete Ex6.py

--- Ex6.py
+++
@@ -1,4 +0,0 @@
-#6. Escreva um programa que leia um valor inteiro e calcule o seu quadrado.
-valor_inteiro = int(input('Digite o valor inteiro a ser calculado: '))
-valor_quadrado = int(valor_inteiro ** 2)
-print(f'O quadrado de {valor_inteiro} é {valor_quadrado}.')
