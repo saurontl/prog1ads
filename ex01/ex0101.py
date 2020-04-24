bb3ee1b0bdabb46c0774505e5c31dda1b717c31c
José Everton <61851698+saurontl@users.noreply.github.com>
2020-04-24 14:29:42 -0300

    Delete ex0101.py

--- ex01/ex0101.py
+++
@@ -1,7 +0,0 @@
-#José Everton da Silva Filho
-#AdS - P1 - Unifip - Patos/PB
-#LISTA - 01
-#---
-#1 - Escreva um programa que recebe o nome do usuário e, em seguida, imprime a frase Olá, nome_do_usuário!.
-nome = str(input('Digite o seu nome: '))
-print('Olá, {}!'.format(nome))
