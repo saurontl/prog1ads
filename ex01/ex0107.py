253704371888230552718f91110685bf755f2d20
José Everton <61851698+saurontl@users.noreply.github.com>
2020-04-24 14:30:48 -0300

    Delete ex0107.py

--- ex01/ex0107.py
+++
@@ -1,8 +0,0 @@
-#José Everton da Silva Filho
-#AdS - P1 - Unifip - Patos/PB
-#LISTA - 01
-#---
-#7 - Escreva um programa que leia um valor inteiro e calcule o seu quadrado.
-valor = int(input('Digite o valor que você deseja calcular o quadrado: '))
-quadrado = valor ** 2
-print('O quadrado de {:.0f} é {:.0f}.'.format(valor, quadrado))
