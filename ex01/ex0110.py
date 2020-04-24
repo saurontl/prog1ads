e74194e157e4094fc2780d53d09c9b546a9e2e37
José Everton <61851698+saurontl@users.noreply.github.com>
2020-04-24 14:30:56 -0300

    Delete ex0110.py

--- ex01/ex0110.py
+++
@@ -1,8 +0,0 @@
-#José Everton da Silva Filho
-#AdS - P1 - Unifip - Patos/PB
-#LISTA - 01
-#---
-#10 - Escreva um programa que converta valores de centímetros em polegadas.
-cm = float(input('Digite o valor em centímetros que você deseja converter para polegadas: '))
-inch = cm / 2.54
-print('{} cm = {} polegadas.'.format(cm, inch))
