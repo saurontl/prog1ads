72ce9a78f37cf57d377d61c5e3055f56610573be
José Everton <61851698+saurontl@users.noreply.github.com>
2020-04-24 14:30:51 -0300

    Delete ex0108.py

--- ex01/ex0108.py
+++
@@ -1,8 +0,0 @@
-#José Everton da Silva Filho
-#AdS - P1 - Unifip - Patos/PB
-#LISTA - 01
-#---
-#8 - Escreva um programa que leia um valor inteiro e calcule o seu cubo.
-valor = int(input('Digite o valor que você seja calcular o cubo: '))
-cubo = valor ** 3
-print('O cubo de {:.0f} é {:.0f}.'.format(valor, cubo))
